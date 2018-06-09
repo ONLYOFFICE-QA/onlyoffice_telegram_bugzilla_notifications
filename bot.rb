3914ea8543c76e6095277fa7ebd9d5ef7ed110cc
Pavel Lobashov <shockwavenn@gmail.com>
2018-06-09 16:43:32 +0300

    Working copy

--- bot.rb
+++
@@ -1,8 +0,0 @@
-require 'telegram/bot'
-require 'yaml'
-
-config = YAML.load_file('config.yml')
-
-Telegram::Bot::Client.run(config['telegram_bot_token']) do |bot|
-  bot.api.sendMessage(chat_id: config['channel_id'], text: 'Hello')
-end
