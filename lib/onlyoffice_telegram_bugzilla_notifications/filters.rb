1327d7d156efd56f3c732502cd5fe6eb44be04d9
l02 <isrLamb@outlook.com>
2024-07-22 16:53:30 +0300

    added documentation for methods of the filters module

--- lib/onlyoffice_telegram_bugzilla_notifications/filters.rb
+++ lib/onlyoffice_telegram_bugzilla_notifications/filters.rb
@@ -3,6 +3,10 @@
 module OnlyofficeTelegramBugzillaNotifications
   # Class to filter bugs by parameter
   class Filters
+    # Initialize the Filters object
+    # @param bugzilla [OnlyofficeBugzillaHelper] instance of bugzilla api
+    # @param config [Hash] Configuration options for filtering
+    # @param bug_id [Integer] ID of the bug to be filter
     def initialize(bugzilla, config, bug_id)
       @bugzilla = bugzilla
       @config = config
@@ -10,10 +14,15 @@ module OnlyofficeTelegramBugzillaNotifications
       @bug_info = @bugzilla.bug_data(@bug_id)
     end
 
+    # Check all filters
+    # @return [Boolean] Result of applying all filters
     def check_all
       by_product
     end
 
+    # Filter bugs by product
+    # @return [Boolean] True if the bug's product matches the configured products,
+    #   or if the product filter is not set, or if the bug's product is not set
     def by_product
       @config['products']&.include?(@bug_info['product']) || @config['products'].nil? || @bug_info['product'].nil?
     end
